ba9fa67c7535dadc948d8330a6152cda39fef2f8
ShireeshPolkam <shireeshpolkam@gmail.com>
2022-12-02 20:12:37 +0530

    location

--- script.js
+++ script.js
@@ -23,3 +23,34 @@ function closeCoupon(){
 
 
 
+
+            function geolocation(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(showPosition)
+                }else{
+                    x.innerText = "Geo Not Supported"
+                }
+            }
+
+            function showPosition(data){
+                console.log(data);
+                let x = document.getElementById('out');
+                let y = document.getElementById('weather');
+                let lat = data.coords.latitude;
+                let long = data.coords.longitude;
+                let out = ``
+                x.innerText = out
+                const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+                ///api calling
+                fetch(url,{method: 'GET'})
+                //return promise
+                .then((res) => res.json())
+                //return data
+                .then((data) => {
+                    console.log(data)
+                    let city = data.city.name
+                    let temp = data.list[0].temp.day
+                    y.innerText = `${city} ${temp} °C`
+                })
+            }
+
